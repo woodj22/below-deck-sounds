ba124c6961f979121ad04df36043ee7d5c3ec06b
JoeWood <joe.wood@gousto.co.uk>
2021-09-13 09:49:24 +0200

    Add google analytics and SEO optimisation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import VueGtag from "vue-gtag";
 
-createApp(App).mount('#app')
+createApp(App).use(VueGtag, {
+    config: { id: "G-VY11NMTK14" }
+}).mount('#app')
